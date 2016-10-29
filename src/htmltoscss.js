62dbad9ad0a9d9c42d75338d5ec4079c61fbfff8
John H Horton <johnhhorton@outlook.com>
2016-10-29 17:32:58 -0400

    Refactoring

--- src/htmltoscss.js
+++ src/htmltoscss.js
@@ -30,65 +30,57 @@ export default class HtmlToScss {
     this.output = null
   }
 
-  hideTags(dom, include = [], exclude = [], all = false) {
-    let newDom = JSON.parse(JSON.stringify(dom))
+  hideTags(dom, include = [], exclude = [], hideAll = false) {
+    let newDom = this.deepCopy(dom)
+
     newDom.forEach(el => {
-      if (all && exclude.indexOf(el.tag) === -1) {
-        el.tag = ''
-      } else if (!all && include.indexOf(el.tag) !== -1) {
-        el.tag = ''
-      }
-      el.children = this.hideTags(el.children, include, exclude, all)
+      let isInExcludeList = (exclude.indexOf(el.tag) !== -1)
+      let isInIncludeList = (include.indexOf(el.tag) !== -1)
+
+      if ((!isInExcludeList && hideAll) || (isInIncludeList && !hideAll)) el.tag = ''
+
+      el.children = this.hideTags(el.children, include, exclude, hideAll)
     })
+
     return newDom
   }
 
   //todo fix this hit
-  hideIds(dom, include = [], exclude = [], all = false) {
-    let newDom = JSON.parse(JSON.stringify(dom))
+  hideIds(dom, include = [], exclude = [], hideAll = false) {
+    let newDom = this.deepCopy(dom)
+
     newDom.forEach(el => {
       el.ids = el.ids.filter(id => {
-        let indexInclude = include.indexOf(id)
-        let indexExclude = exclude.indexOf(id)
-        if (all && indexExclude === -1) {
-          return false
-            //el.ids = el.ids.splice(indexExclude, 1)
-        } else if (!all && indexInclude !== -1) {
-          return false
-            //el.ids = el.ids.splice(indexInclude, 1)
-        }
-        return true
-
+        let isInExcludeList = (exclude.indexOf(id) !== -1)
+        let isInIncludeList = (include.indexOf(id) !== -1)
+        return !((hideAll && isInExcludeList) || (!hideAll && isInIncludeList))
       })
-      el.children = this.hideIds(el.children, include, exclude, all)
+
+      el.children = this.hideIds(el.children, include, exclude, hideAll)
     })
     return newDom
   }
 
   //todo fix this hit
-  hideClasses(dom, include = [], exclude = [], all = false) {
-    let newDom = JSON.parse(JSON.stringify(dom))
+  hideClasses(dom, include = [], exclude = [], hideAll = false) {
+    let newDom = this.deepCopy(dom)
+
     newDom.forEach(el => {
       el.classes = el.classes.filter(theClass => {
-        let indexInclude = include.indexOf(theClass)
-        let indexExclude = exclude.indexOf(theClass)
-        if (all && indexExclude === -1) {
-          return false
-            //el.classes = el.classes.splice(indexExclude, 1)
-        } else if (!all && indexInclude !== -1) {
-          return false
-            //el.classes = el.classes.splice(indexInclude, 1)
-        }
-        return true
-
+        let isInExcludeList = (exclude.indexOf(theClass) !== -1)
+        let isInIncludeList = (include.indexOf(theClass) !== -1)
+        return !((hideAll && isInExcludeList) || (!hideAll && isInIncludeList))
       })
-      el.children = this.hideClasses(el.children, include, exclude, all)
+
+      el.children = this.hideClasses(el.children, include, exclude, hideAll)
     })
+
     return newDom
   }
 
   hideElementsByClass(dom, hideList = []) {
-    let newDom = JSON.parse(JSON.stringify(dom))
+    let newDom = this.deepCopy(dom)
+
     newDom.forEach(el => {
       el.classes.forEach(theClass => {
         if (hideList.indexOf(theClass) !== -1) {
@@ -103,7 +95,7 @@ export default class HtmlToScss {
   }
 
   hideElementsById(dom, hideList = []) {
-    let newDom = JSON.parse(JSON.stringify(dom))
+    let newDom = this.deepCopy(dom)
     newDom.forEach(el => {
       el.ids.forEach(id => {
         if (hideList.indexOf(id) !== -1) {
@@ -118,7 +110,7 @@ export default class HtmlToScss {
   }
 
   removeElementsByClass(dom, removeList = []) {
-    let newDom = JSON.parse(JSON.stringify(dom))
+    let newDom = this.deepCopy(dom)
     let remove = false
     newDom = newDom.filter((el) => {
       let keep = true
@@ -137,7 +129,7 @@ export default class HtmlToScss {
   }
 
   removeElementsById(dom, removeList = []) {
-    let newDom = JSON.parse(JSON.stringify(dom))
+    let newDom = this.deepCopy(dom)
     let remove = false
     newDom = newDom.filter((el) => {
       let keep = true
@@ -156,7 +148,7 @@ export default class HtmlToScss {
   }
 
   extractByClass(dom, extractList = []) {
-    let newDom = JSON.parse(JSON.stringify(dom))
+    let newDom = this.deepCopy(dom)
     let domLength = newDom.length - 1
     let extracted = []
 
@@ -184,7 +176,7 @@ export default class HtmlToScss {
     return newDom
   }
   extractById(dom, extractList = []) {
-    let newDom = JSON.parse(JSON.stringify(dom))
+    let newDom = this.deepCopy(dom)
     let domLength = newDom.length - 1
     let extracted = []
 
@@ -213,7 +205,7 @@ export default class HtmlToScss {
   }
 
   reduceSiblings(dom) {
-    let newDom = JSON.parse(JSON.stringify(dom))
+    let newDom = this.deepCopy(dom)
     let removeIndexes = []
     if (newDom.length > 1) {
       newDom = newDom.filter((el, index, arr) => {
@@ -234,7 +226,7 @@ export default class HtmlToScss {
   }
 
   combineSimilarParents(dom) {
-    let newDom = JSON.parse(JSON.stringify(dom))
+    let newDom = this.deepCopy(dom)
     if (newDom.length > 1) {
       newDom = newDom.filter((el, index, arr) => {
         let filter = true
@@ -257,7 +249,7 @@ export default class HtmlToScss {
 
   convertBEM(dom, parentString) {
 
-    let newDom = JSON.parse(JSON.stringify(dom))
+    let newDom = this.deepCopy(dom)
 
     newDom = newDom.map((el) => this.convertBEMRecursive(el))
 
@@ -270,36 +262,36 @@ export default class HtmlToScss {
 
     let bemParentClassIndex = -1
     parent.children.forEach((child) => {
-        let isBemChild = false
-        let bemClassIndex = -1
-        child.classes = child.classes.map((childClass, childClassIndex) => {
-          let childClassOutput = childClass
-          parent.classes.forEach((parentClass, index) => {
-            if (childClassOutput.indexOf(parentClass + '__') !== -1) {
-              isBemChild = true
-              childClassOutput = childClassOutput.replace(parentClass + '__', '&__')
-              bemParentClassIndex = index
-              bemClassIndex = childClassIndex
-            }
-          })
-          return childClassOutput
-        })
-
-        if (isBemChild) {
-          child.tag = ''
-          if (bemClassIndex > 0) {
-            let bemClass = child.classes[bemClassIndex]
-            let newClasses = []
-            child.classes = child.classes.slice(bemClassIndex, bemClassIndex)
-            
-            newClasses.push(bemClass)
-            // newClasses.concat(child.classes)
-            child.classes= newClasses
+      let isBemChild = false
+      let bemClassIndex = -1
+      child.classes = child.classes.map((childClass, childClassIndex) => {
+        let childClassOutput = childClass
+        parent.classes.forEach((parentClass, index) => {
+          if (childClassOutput.indexOf(parentClass + '__') !== -1) {
+            isBemChild = true
+            childClassOutput = childClassOutput.replace(parentClass + '__', '&__')
+            bemParentClassIndex = index
+            bemClassIndex = childClassIndex
           }
-
-        }
+        })
+        return childClassOutput
       })
-      //move bem parent to end of array
+
+      if (isBemChild) {
+        child.tag = ''
+        if (bemClassIndex > 0) {
+          let bemClass = child.classes[bemClassIndex]
+          let newClasses = []
+          child.classes = child.classes.slice(bemClassIndex, bemClassIndex)
+
+          newClasses.push(bemClass)
+          // newClasses.concat(child.classes)
+          child.classes = newClasses
+        }
+
+      }
+    })
+    //move bem parent to end of array
     if (bemParentClassIndex > 0) {
       let bemParent = parent.classes[bemParentClassIndex]
       parent.classes = parent.classes.slice(bemParentClassIndex, bemParentClassIndex)
@@ -330,6 +322,14 @@ export default class HtmlToScss {
 
   }
 
+  emptyElementValues(el) {
+    let newElement = this.deepCopy(el)
+    newElement.tag = ''
+    newElement.ids = []
+    newElement.classes = []
+    return newElement
+  }
+
   toArray(obj) {
     var array = [];
     // iterate backwards ensuring that length is an UInt32
@@ -337,6 +337,10 @@ export default class HtmlToScss {
       array[i] = obj[i];
     }
     return array;
+  }
+
+  deepCopy(obj) {
+    return JSON.parse(JSON.stringify(obj))
   }
 
   extractHtml(inputHtml) {
